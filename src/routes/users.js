8676ba263f8ca82c583de1098372b0c58232a6f5
anji-braimoh <anjolabraimoh@outlook.com>
2022-11-11 11:53:45 +0000

    added more functionality to CRUD operations

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,21 +1,40 @@
 const {Router} = require ('express')
 const userRouter = Router()
 const User = require ("../../models/User")
+const Shows = require ("../models/Show")
 
 userRouter.get("/",async(req,res) => {
-    res.send("Successful GET Request made!")
+    const users = await User.findAll()
+    res.status(200).send(shows)
 })
 
-userRouter.get("/id",async (req,res) => {
-    res.send("Successful GET Request made!")
+userRouter.get("/:id",async (req,res) => {
+    const user = await Users.findByPk(req.params.id)
+    if (user === true){
+        res.status(200).send(show)
+    }else{ res.sendStatus(404)
+    }
 })
 
-userRouter.get("/id/shows",async (req,res) => {
-    res.send("Successful GET Request made!")
+userRouter.get("/:id/:shows",async (req,res) => {
+    const user = await User.findByPk(req.params.id)
+    if (user === true){
+        const show = await user.getShows()
+    res.status(200).send(show)
+    }else { res.status(404).send("error")
+    }
 })
 
-userRouter.put("/id/shows",async (req,res) => {
-    res.send("Successful PUT Request made!")
+userRouter.put("/:id/:shows",async (req,res) => {
+   const user = await User.findByPk(req.params.id)
+   const show = await Shows.findByPk(req.params.id)
+
+   if (user && show === true){
+   await user.addShow(show)
+   res.status(200).send("Success")
+}
+else {res.status(404).send("error")}
+
 })
 
 module.exports = userRouter
